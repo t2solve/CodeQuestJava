ab14fb084386da9f15136af9137a4aa34693637d
MCBurner <marcus.brenner@rocketmail.com>
2025-02-03 15:11:36 +0100

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
-class HelloWorld{  
+import java.net.StandardSocketOptions;
+
+class HelloWorld{
         public static void main(String args[]){  
-         System.out.println("Hello Java");  
+         System.out.println("Hello Java");
+         System.out.println("your world");
         }  
 }  
